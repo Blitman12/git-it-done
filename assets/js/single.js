70c0caf57fc5fadb609c5fe74b9df9cffd7bb9a6
B-Litty <bradlitman94@gmail.com>
2021-10-10 10:22:11 -0700

    warning for > 30 issues created

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,28 +1,28 @@
-var issueContainerEl = document.querySelector("#issues-container");
-
+let issueContainerEl = document.querySelector("#issues-container");
+let limitWarningEl = document.querySelector("#limit-warning");
 
 let displayIssues = function (issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
     }
-    
-    for (var i = 0; i < issues.length; i++) {
+
+    for (let i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
-        var issueEl = document.createElement("a");
+        let issueEl = document.createElement("a");
         issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
 
         // create span to hold issue title
-        var titleEl = document.createElement("span");
+        let titleEl = document.createElement("span");
         titleEl.textContent = issues[i].title;
 
         // append to container
         issueEl.appendChild(titleEl);
 
         // create a type element
-        var typeEl = document.createElement("span");
+        let typeEl = document.createElement("span");
 
         // check if issue is an actual issue or a pull request
         if (issues[i].pull_request) {
@@ -38,6 +38,18 @@ let displayIssues = function (issues) {
     }
 };
 
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    let linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 let getRepoIssues = function (repo) {
     let apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
@@ -45,6 +57,11 @@ let getRepoIssues = function (repo) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo)
+                }
             });
         } else {
             alert("There was a problem with your request.")
